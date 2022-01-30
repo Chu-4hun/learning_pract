b1461e2aa864893706af3607d47631a9b8d3e49f
Chu-4hun <tapok134224@mail.ru>
2022-01-30 15:55:27 +0300

    add groups_page.xaml, otdel_page.xaml, and edit teacher_page.xaml

---
+++ pages/admin_pages/groups_page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace learning_pract.pages.admin_pages
+{
+    public partial class groups_page : Page
+    {
+        public groups_page()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ pages/admin_pages/otdel_page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace learning_pract.pages.admin_pages
+{
+    public partial class otdel_page : Page
+    {
+        public otdel_page()
+        {
+            InitializeComponent();
+        }
+    }
+}
