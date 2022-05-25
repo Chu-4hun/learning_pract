007d0b7ee7268fb6ceb57373322ee8d8c808dafc
Chu-4hun <tapok134224@mail.ru>
2022-05-25 23:26:14 +0300

    Export to Exel and new page

--- pages/user_pages/audit_fund_page.xaml.cs
+++ pages/user_pages/audit_fund_page.xaml.cs
@@ -3,9 +3,13 @@ using System.Windows;
 using System.Windows.Controls;
 using learning_pract.Models;
 
+using Excel = Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Interop.Excel;
+
+
 namespace learning_pract.pages.user_pages
 {
-    public partial class audit_fund_page : Page
+    public partial class audit_fund_page : Excel.Page
     {
         List<Audit> audit = new List<Audit>();
 
@@ -44,9 +48,40 @@ namespace learning_pract.pages.user_pages
             UpdateTable(Schedule.getAll());
         }
 
-        private void EditButton_OnClick(object sender, RoutedEventArgs e)
+        private void AddButton_OnClick(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("In development");
+            add_schedule_page addSchedulePage = new add_schedule_page();
+            if (NavigationService != null) NavigationService.Navigate(addSchedulePage);
         }
+
+        private void ExportButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Excel.Application excel = new Excel.Application();
+            excel.Visible = true; 
+            Workbook workbook = excel.Workbooks.Add(System.Reflection.Missing.Value);
+            Worksheet sheet1 = (Worksheet)workbook.Sheets[1];
+ 
+            for (int j = 0; j < AuditGrid.Columns.Count; j++) 
+            {
+                Range myRange = (Range)sheet1.Cells[1, j + 1];
+                myRange.Value2 = AuditGrid.Columns[j].Header;
+            }
+            for (int i = 0; i < AuditGrid.Columns.Count; i++)
+            {
+                for (int j = 0; j < AuditGrid.Items.Count; j++)
+                {
+                    TextBlock b = AuditGrid.Columns[i].GetCellContent(AuditGrid.Items[j]) as TextBlock;
+                    Microsoft.Office.Interop.Excel.Range myRange = (Microsoft.Office.Interop.Excel.Range)sheet1.Cells[j + 2, i + 1];
+                    myRange.Value2 = b.Text;
+                }
+            }
+        }
+
+        public HeaderFooter LeftHeader { get; }
+        public HeaderFooter CenterHeader { get; }
+        public HeaderFooter RightHeader { get; }
+        public HeaderFooter LeftFooter { get; }
+        public HeaderFooter CenterFooter { get; }
+        public HeaderFooter RightFooter { get; }
     }
 }
